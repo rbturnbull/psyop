95b1cfd258c2c8864931bc1c8285997cdc926238
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2025-09-05 10:14:37 +1000

    :cop: adding tests for optimal'

--- tests/test_opt.py
+++ tests/test_opt.py
@@ -1,7 +1,6 @@
-from pathlib import Path
 import pandas as pd
 
-from psyop.opt import suggest_candidates
+from psyop.opt import suggest_candidates, find_optimal
 
 from .test_model import TEST_DATA_DIR
 TEST_XARRAY = TEST_DATA_DIR/"trials.nc"
@@ -13,3 +12,10 @@ def test_suggest_candidates(tmpdir):
     df = pd.read_csv(output_path)
     assert len(df) == 12
 
+
+def test_find_optimal(tmpdir):
+    output_path = tmpdir/"output.csv"
+    find_optimal(TEST_XARRAY, output_path=output_path)
+    assert output_path.exists()
+    df = pd.read_csv(output_path)
+    assert len(df) == 10
