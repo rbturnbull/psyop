57c80692ab4263a67f517d04da99fadd5761abe7
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2025-10-28 15:20:15 +1100

    :cop: adding tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -14,6 +14,13 @@ def test_app_model(tmpdir):
     assert output.exists()
 
 
+def test_app_model_prior(tmpdir):
+    output = tmpdir/"output.nc"
+    result = runner.invoke(app, ["model", str(TEST_CSV), str(output), "--target", "loss", "--exclude", "trial_id", "--prior-model", str(TEST_XARRAY)])
+    assert result.exit_code == 0
+    assert output.exists()
+
+
 def test_app_suggest(tmpdir):
     output = tmpdir/"output.csv"
     result = runner.invoke(app, ["suggest", str(TEST_XARRAY), "--output", str(output)])
@@ -47,3 +54,12 @@ def test_app_plot1d(tmpdir):
     result = runner.invoke(app, ["plot1d", str(TEST_XARRAY), "--output", str(output_path)])
     assert result.exit_code == 0
     assert output_path.exists()
+
+
+def test_export(tmpdir):
+    output = tmpdir/"output.csv"
+    result = runner.invoke(app, ["export", str(TEST_XARRAY), str(output)])
+    assert result.exit_code == 0
+    assert output.exists()
+    text = output.read_text('utf8')
+    assert text.startswith('trial_id,batch_size,dropout,epochs,learning_rate,r_drop_alpha,loss\n1,16,')
